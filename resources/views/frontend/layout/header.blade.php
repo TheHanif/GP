738df7851027a35a0a45371326d89fd1c03e874c
Muhammad Hanif Mianjee <developer.hanif@gmail.com>
2017-02-07 22:33:03 +0500

    Added toolbar markup

--- resources/views/frontend/layout/header.blade.php
+++ resources/views/frontend/layout/header.blade.php
@@ -1,4 +1,39 @@
 <header>
+	
+	<div class="container-fluide toolbar">
+		<div class="container">
+			
+			<nav class="navbar" role="navigation">
+				
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<span class="navbar-text">Support 24/7 : 03000287523</span>
+				</div>
+		
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse navbar-ex1-collapse">
+					
+					<ul class="nav navbar-nav navbar-right">
+						<li><a href="#">My Account</a></li>
+						<li><a href="#">Wishlist</a></li>
+						<li><a href="#">Checkout</a></li>
+						<li><a href="#">Cart</a></li>
+						<li><a href="#">Track my order</a></li>
+						<li><a href="#">Help</a></li>
+					</ul>
+				</div><!-- /.navbar-collapse -->
+
+			</nav>
+
+		</div>
+	</div>{{-- / .toolbar --}}
+
 	<div class="container">
 		This is header
 	</div>{{-- / .container --}}
